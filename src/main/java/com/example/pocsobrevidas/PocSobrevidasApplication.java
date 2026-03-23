108a8e9959e3b173f3c0f3946057e6da31def0db
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-23 20:06:37 -0300

    Initial commit

---
+++ src/main/java/com/example/pocsobrevidas/PocSobrevidasApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocsobrevidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSobrevidasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocSobrevidasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pocsobrevidas/PocSobrevidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocsobrevidas;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class PocSobrevidasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
