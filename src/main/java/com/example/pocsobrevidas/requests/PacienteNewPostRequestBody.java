d10d76283bfdebd540cb149e792d2fed2eb6f68c
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-28 16:43:13 -0300

    add: criando pacote request e adicionando um DTO para o metodo POST

---
+++ src/main/java/com/example/pocsobrevidas/requests/PacienteNewPostRequestBody.java
@@ -0,0 +1,33 @@
+package com.example.pocsobrevidas.requests;
+
+
+import com.example.pocsobrevidas.domain.PacienteEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PacienteNewPostRequestBody {
+    private LocalDateTime dataNascimento;
+    private String cpf;
+    private String cep;
+    private String numCartaoSus;
+    private String telefoneCelular;
+    private String telefoneResponsavel;
+    private String nomeMae;
+    private String complemento;
+    private String email;
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private PacienteEnum sexo;
+    private String endereco;
+    private Integer numEndereco;
+    private String estado;
+    private String cidade;
+    private Boolean ehTabagista;
+    private Boolean ehEtilista;
+    private Boolean temLesaoSuspeita;
+    private String bairro;
+    private Boolean participaSmartMonitor;
+}
