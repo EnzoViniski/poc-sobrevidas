ba674da9454d9f317a1950a2a56826b858cf15b7
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-28 16:44:13 -0300

    add: adicionando um método para a interface do repository (findByCpf)

--- src/main/java/com/example/pocsobrevidas/repository/PacienteRepository.java
+++ src/main/java/com/example/pocsobrevidas/repository/PacienteRepository.java
@@ -3,5 +3,8 @@ package com.example.pocsobrevidas.repository;
 import com.example.pocsobrevidas.domain.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> findByCpf(String cpf);
 }
