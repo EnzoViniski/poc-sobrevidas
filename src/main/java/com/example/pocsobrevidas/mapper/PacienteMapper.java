858b6a9caecfc548ea18fbaf86797bc6c573a342
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-28 16:42:44 -0300

    add: criando mapper para metodo Post (save) e Put (update)

---
+++ src/main/java/com/example/pocsobrevidas/mapper/PacienteMapper.java
@@ -0,0 +1,16 @@
+package com.example.pocsobrevidas.mapper;
+
+import com.example.pocsobrevidas.domain.Paciente;
+import com.example.pocsobrevidas.requests.PacienteNewPostRequestBody;
+import com.example.pocsobrevidas.requests.PacientePutRequestBody;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public abstract class PacienteMapper {
+    public static final PacienteMapper INSTANCE = Mappers.getMapper(PacienteMapper.class);
+
+    public abstract Paciente toPaciente(PacienteNewPostRequestBody pacienteNewPostRequestBody);
+
+    public abstract Paciente toPaciente(PacientePutRequestBody pacientePutRequestBody);
+}
