891a127e21048e4c0e3132c2201427a2b9281750
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-28 16:40:44 -0300

    add: adicionando Builder

--- src/main/java/com/example/pocsobrevidas/domain/Paciente.java
+++ src/main/java/com/example/pocsobrevidas/domain/Paciente.java
@@ -2,6 +2,7 @@ package com.example.pocsobrevidas.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Paciente {
     private LocalDateTime dataNascimento;
     @Id
