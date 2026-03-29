01e4f3025637b6c0f95594f279885876a23215cb
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-29 11:22:44 -0300

    add: adicionando documentação para swagger

--- src/main/java/com/example/pocsobrevidas/controller/PacienteController.java
+++ src/main/java/com/example/pocsobrevidas/controller/PacienteController.java
@@ -4,6 +4,7 @@ import com.example.pocsobrevidas.domain.Paciente;
 import com.example.pocsobrevidas.requests.PacienteNewPostRequestBody;
 import com.example.pocsobrevidas.requests.PacientePutRequestBody;
 import com.example.pocsobrevidas.service.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,12 @@ import java.util.List;
 @RequestMapping("/pacientes")
 public class PacienteController {
     private final PacienteService pacienteService;
+
     public PacienteController(PacienteService pacienteService) {
         this.pacienteService = pacienteService;
     }
 
+    @Operation(description = "Busca todos os pacientes")
     @GetMapping
     public ResponseEntity<List<Paciente>> list() {
         return ResponseEntity.ok(pacienteService.list());
@@ -30,28 +33,33 @@ public class PacienteController {
         return ResponseEntity.ok(pacienteService.findById(id));
     }
 
+    @Operation(description = "Busca um paciente pelo CPF")
     @GetMapping(path = "/cpf/{cpf}")
     public ResponseEntity<Paciente> findByCpf(@PathVariable String cpf) {
         return ResponseEntity.ok(pacienteService.findByCpf(cpf));
     }
 
+    @Operation(description = "Adiciona um novo paciente")
     @PostMapping
     public ResponseEntity<Paciente> save(@RequestBody PacienteNewPostRequestBody paciente) {
         return new ResponseEntity<>(pacienteService.save(paciente), HttpStatus.CREATED);
     }
 
+    @Operation(description = "Importa o arquivo CSV pro banco de dados")
     @PostMapping("/importar")
     public ResponseEntity<String> importarArquivo(@RequestParam("file") MultipartFile file) {
         pacienteService.importarCsv(file);
         return ResponseEntity.ok("Arquivo importado com sucesso: " + file.getOriginalFilename() + "!");
     }
 
+    @Operation(description = "Deleta um paciente pelo ID")
     @DeleteMapping(path = "{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         pacienteService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(description = "Atualiza os dados de um paciente pelo ID")
     @PutMapping(path = "{id}")
     public ResponseEntity<Paciente> replace(@PathVariable long id, @RequestBody PacientePutRequestBody paciente) {
         pacienteService.replace(id, paciente);
